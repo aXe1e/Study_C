9222d1cca2988dad9c8a136062fadd545ed91daf
aXe1e <axele@list.ru>
2021-03-02 18:51:55 +0500

    Домашнее задание по 5-у уроку.

--- Lesson-005/Program.cs
+++ Lesson-005/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lesson_005
 {
@@ -7,11 +8,30 @@ namespace Lesson_005
         static void Main(string[] args)
         {
             //1.Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+            /*
+            Console.WriteLine("Введите произвольный набор данных:");
+            string s = Console.ReadLine();
+            string filename = "text.txt";
+            File.WriteAllText(filename, s);
+            */
             
+            /*
             //2.Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
-            
-            //3.Ввести с клавиатуры произвольный набор чисел(0...255) и записать их в бинарный файл.
+            File.AppendAllText("startup.txt", $"{DateTime.Now:T}\n");
+            */
 
+            /*
+            //3.Ввести с клавиатуры произвольный набор чисел(0...255) и записать их в бинарный файл.
+            string filename = "Numbers.bin";
+            byte[] bForFile = new byte[5];
+            
+            Console.WriteLine("Введите с клавиатуры произвольный набор чисел(0...255).");
+            for (int i = 0; i < bForFile.Length; i++)
+            {
+                bForFile[i] = Convert.ToByte(Console.ReadLine());
+            }
+            File.WriteAllBytes(filename, bForFile);    
+            */       
         }
     }
 }
